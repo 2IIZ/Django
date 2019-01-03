22470708223edff0d76f5b90d3f385197343ea03
Unknown <2IIZ1998@gmail.com>
2019-01-03 16:36:51 +0100

    ex: word count from a text

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,9 +1,10 @@
 # @Date:   2019-01-03T11:34:37+01:00
-# @Last modified time: 2019-01-03T16:00:59+01:00
+# @Last modified time: 2019-01-03T16:33:23+01:00
 
 #return some info into http response
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 #every time someone come visit the page it will send this request
 def home(request):
@@ -11,7 +12,27 @@ def home(request):
 	return render(request, 'home.html')
 
 def count(request):
-	return render(request, 'count.html')
+	#this retrieve the text from the form fulltext
+	fulltext = request.GET['fulltext']
+
+	#every word is splited in one case
+	wordlist = fulltext.split()
+
+	worddictionary = {}
+	#loop throug each word
+	for word in wordlist:
+		if word in worddictionary:
+			#increase the dictionary
+			worddictionary[word] += 1
+		else:
+			#add to the dictionary
+			worddictionary[word] = 1
+
+	sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+	#					,				, I can use fulltext in count.html -> this will give me the value
+	return render(request, 'count.html', {'fulltext':fulltext, 'countword':len(wordlist), 'sortedwords':sortedwords})
+
 
 
 def goat(request):

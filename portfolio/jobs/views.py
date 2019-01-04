f61d167432256c6fc979ff380d4cefe2be69edf5
Unknown <2IIZ1998@gmail.com>
2019-01-04 17:12:53 +0100

    showing jobs from database with for loop

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,12 +1,14 @@
 # @Date:   2019-01-04T09:55:12+01:00
-# @Last modified time: 2019-01-04T16:20:10+01:00
+# @Last modified time: 2019-01-04T17:00:47+01:00
 
 
 
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+
 # templates/jobs/.html
 #	the engine automatically know where to found the /templates from the views.py-->
 def home(request):
-	return render(request, 'jobs/home.html')
+	jobs = Job.objects #will retrieve all data from the job database
+	return render(request, 'jobs/home.html', {"jobs":jobs})

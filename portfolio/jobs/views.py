3f56915d939eb4c3c86fc83d0e73ee00a26f6f76
Unknown <2IIZ1998@gmail.com>
2019-01-04 16:48:15 +0100

    bootstrap added

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,10 +1,12 @@
 # @Date:   2019-01-04T09:55:12+01:00
-# @Last modified time: 2019-01-04T16:09:33+01:00
+# @Last modified time: 2019-01-04T16:20:10+01:00
 
 
 
 from django.shortcuts import render
 
 # Create your views here.
+# templates/jobs/.html
+#	the engine automatically know where to found the /templates from the views.py-->
 def home(request):
 	return render(request, 'jobs/home.html')

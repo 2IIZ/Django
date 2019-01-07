08fadbc213d5c24dced2a8a7dfce93bb670101dc
Unknown <2IIZ1998@gmail.com>
2019-01-07 19:22:31 +0100

    Added requirements.txt for pip

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -1,5 +1,5 @@
 # @Date:   2019-01-03T17:30:45+01:00
-# @Last modified time: 2019-01-07T16:37:33+01:00
+# @Last modified time: 2019-01-07T16:51:33+01:00
 
 
 
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio',
-		'USER': 'postgres',
+		'USER': 'iv',
 		'PASSWORD': ' ',
 		'HOST': '127.0.0.1',
 		'PORT': '5432',

ecf326a67f255e5563f368d5cdec20cabd046d71
Unknown <2IIZ1998@gmail.com>
2019-01-10 14:45:42 +0100

    first running script ok

---
+++ webscraping/scripts/parsing.py
@@ -0,0 +1,7 @@
+# Create a script : https://django-extensions.readthedocs.io/en/latest/runscript.html
+
+# to run this in the shell, you'll have to install "django-extensions"
+# add this to the apps settings. 'django_extensions',
+
+def run():
+    print("hello")

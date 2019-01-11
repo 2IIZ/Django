16b66f5c1357c93766bf4085add2109a21c28855
Unknown <2IIZ1998@gmail.com>
2019-01-11 12:11:49 +0100

    First parsing

--- webscraping/scripts/parsing.py
+++ webscraping/scripts/parsing.py
@@ -1,7 +1,21 @@
 # Create a script : https://django-extensions.readthedocs.io/en/latest/runscript.html
 
-# to run this in the shell, you'll have to install "django-extensions"
+# pip install django-extensions
 # add this to the apps settings. 'django_extensions',
 
+# pip install BeautifulSoup4
+
+#import libraries
+from urllib.request import urlopen # to get the HTML page of an url.
+from bs4 import BeautifulSoup
+
 def run():
-    print("hello")
+
+    markup = "<b><!--Hey, buddy. Want to buy a used parser?--></b>"
+
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(markup)
+
+    name_box = soup.b.string
+
+    print(name_box)

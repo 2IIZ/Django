cda54298789a47bac7e4051c5c6a087989754aff
Unknown <2IIZ1998@gmail.com>
2019-01-29 15:14:26 +0100

    Product model

--- 2_projecthunt/products/models.py
+++ 2_projecthunt/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    pub_date = models.DateTimeField()
+    total_votes = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self): #that show the particular object in admin section
+    	return self.title

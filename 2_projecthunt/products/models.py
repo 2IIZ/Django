2a840bb6a4c52b02c8294471eed303b29f140c04
Unknown <2IIZ1998@gmail.com>
2019-01-30 11:06:08 +0100

    Save product to db

--- 2_projecthunt/products/models.py
+++ 2_projecthunt/products/models.py
@@ -3,15 +3,21 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    url = models.URLField()
+    body = models.TextField()
+    url = models.TextField()
     pub_date = models.DateTimeField()
     total_votes = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
-    body = models.TextField()
 
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self): #that show the particular object in admin section
     	return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
